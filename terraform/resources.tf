c21956dfcff63a0a45c6d973a442c64c20626740
Kevin Printz <kevinprintz@microsoft.com>
2023-02-23 11:42:07 -0500

    fix

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -35,7 +35,7 @@ resource "azurerm_network_interface" "nic" {
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = data.azurerm_subnet.subnet.id
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.pip.id
   }
